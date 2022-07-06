478db55f155bf28207d36e59a037d47fd2749d2b
Michael Lydeamore <michael.lydeamore@monash.edu>
2022-07-06 13:45:02 +0700

    add documentation for member markdown generator

--- R/generate_markdown_for_member.R
+++ R/generate_markdown_for_member.R
@@ -1,3 +1,11 @@
+
+#' Generate markdown for staff member
+#'
+#' Generates the image, short description and linking-out buttons for use in
+#' a list of staff belonging to a group.
+#'
+#' @param member An object of class `staff_member`.
+#'
 generate_markdown_for_member <- function(member) {
 
     if (!"staff_member" %in% class(member)) {
