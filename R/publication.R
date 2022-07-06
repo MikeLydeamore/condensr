19bade1461245f504bcdb8b371b8b1917bd6812a
Michael Lydeamore <michael.lydeamore@monash.edu>
2022-07-06 10:23:36 +0700

    add documentation to publication

--- R/publication.R
+++ R/publication.R
@@ -1,8 +1,41 @@
+#' Generate publication
+#'
+#' Generates a publication object that is linked from staff websites
+#'
+#' @param title Title of the publication.
+#' @param related_staff Vector of the `id`s of the related staff. 
+#' @param link URL of the publication that will be linked.
+#' @param citation Text that will be displayed for this publication.
+#' @param date Date of publication. List of publications is sorted on the staff pages.
+#'
+#' @return An object of class `publication` with 5 elements:
+#' * `title`
+#' * `related_staff`
+#' * `link`
+#' * `citation`
+#' * `date`
+#' 
+#' @examples
+#' # example_project <- publication(
+#'        title = "Joe's Blogging Paper",
+#'        related_staff = c("joe-bloggs"),
+#'        link = NA,
+#'        citation = "Bloggs, J., Bloggy, Y. (2019), The power of blogging, Australian Journal of Bloggs, 43: 149-155.",
+#'        date = as.Date("2019-01-01")
+#'    )
+#' 
 publication <- function(title,
                         related_staff,
                         link,
                         citation,
                         date) {
+    
+    assertthat::assert_that(assertthat::is.string(title))
+    assertthat::assert_that(is.vector(related_staff))
+    assertthat::assert_that(assertthat::is.string(link))
+    assertthat::assert_that(assertthat::is.string(citation))
+    assertthat::assert_that(assertthat::is.date(date))
+    
     pub <- list(
         title = title,
         related_staff = related_staff,
