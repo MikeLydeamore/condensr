ff2dc3209c581c265e11363c46bbe6a6557af1ce
Michael Lydeamore <michael.lydeamore@monash.edu>
2022-07-13 16:07:28 +1000

    add staff markdown template generator

--- tests/testthat/test-site-generator.R
+++ tests/testthat/test-site-generator.R
@@ -1,9 +1,9 @@
 test_that("test site generates", {
     test_site_path <- file.path(test_path(), "test_site")
-    unlink(test_site_path, recursive = T) #Remove test_site folder
-    
+    unlink(test_site_path, recursive = T) # Remove test_site folder
+
     create_website(dir = test_site_path, title = "test_site")
-    
+
     expect_true(dir.exists(test_site_path))
 })
 
@@ -23,4 +23,27 @@ test_that("render template generates a given path", {
 
     expect_true(dir.exists(test_site_path))
     expect_true(file.exists(file.path(test_site_path, "_site.yml")))
+})
+
+test_that("staff template appears", {
+    test_site_path <- file.path(test_path(), "test_site")
+    example_staff_member <- staff_member(
+        id = "example-id",
+        name = "Example Name",
+        description = "Example description for example staff member.",
+        external_link = "http://www.example.com/",
+        internal_link = FALSE
+    )
+
+    create_staff_member_page(
+        staff_member = example_staff_member,
+        site_dir = test_site_path,
+        staff_subdir = "staff"
+    )
+
+    expect_true(file.exists(file.path(test_site_path, "staff/example-id.Rmd")))
+})
+
+test_that("staff inputs are valid", {
+    expect_error(create_staff_member_page(list(), site_dir = "test_site"))
 })
