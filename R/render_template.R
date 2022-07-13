77a60a8f89023932f1fd86f0af55858eab3ff27d
Michael Lydeamore <michael.lydeamore@monash.edu>
2022-07-13 16:01:13 +1000

    move output_name to top of render_template

--- R/render_template.R
+++ R/render_template.R
@@ -12,7 +12,11 @@
 #' and `title`.
 #'
 render_template <- function(file, template_folder, target_path, output_name = NA, data = list()) {
-    message("Creating ", file.path(target_path, template_folder, file))
+    if (is.na(output_name)) {
+        output_name <- file
+    }
+
+    message("Creating ", file.path(target_path, template_folder, output_name))
     template <- system.file(file.path("rmarkdown", "templates", template_folder, file),
         package = "condensr"
     )
@@ -22,9 +26,6 @@ render_template <- function(file, template_folder, target_path, output_name = NA
         dir.create(target_path, recursive = TRUE, showWarnings = FALSE)
     }
 
-    if (is.na(output_name)) {
-        output_name <- file
-    }
 
     writeLines(output, file.path(target_path, output_name), useBytes = TRUE)
 }
